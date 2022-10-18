c335494b36a6af5a2f0287700bc229ab8dde95bf
Tim Cuddeback <cuddebtj@gmail.com>
2022-10-17 19:13:28 -0600

    Before test run

--- mom_weekly_rankings_email/app.py
+++ mom_weekly_rankings_email/app.py
@@ -1,9 +1,14 @@
 import yaml
 from pathlib import Path
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
 
 import base64
-import googleapiclient.discovery
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -15,36 +20,44 @@ with open(PRIVATE_YAML) as file:
     private = yaml.load(file, Loader=yaml.SafeLoader)
 
 emails = private['email_list']
+creds = None
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.send',
           'https://www.googleapis.com/auth/gmail.modify']
 
-flow = InstalledAppFlow.from_client_secrets_file(G_OAUTH_CLIENT, SCOPES)
+if CREDS_PATH.is_file():
+    creds = Credentials.from_authorized_user_file(CREDS_PATH, SCOPES)
 
-creds = flow.run_local_server(port=0)
+if not creds and not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+            creds.refresn(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(G_OAUTH_CLIENT, SCOPES)
+    with open(CREDS_PATH, 'w') as token:
+        yaml.dump(creds, token)
 
-print(creds)
+try:
+    service = build('gmail', 'v1', credentials=creds)
 
-with open(CREDS_PATH, 'wb') as token:
-    yaml.dump(creds, token)
+    my_email = 'menofmadisonffble@gmail.com'
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = 'MoM FFBL Weekly Rankings'
+    msg['From'] = f'{my_email}'
+    msg['To'] = f'cuddebtj@gmail.com'
+    msgPlain = 'This is my first email!'
+    msgHtml = '<b>This is my first email!</b>'
+    msg.attach(MIMEText(msgPlain, 'plain'))
+    msg.attach(MIMEText(msgHtml, 'html'))
+    raw = base64.urlsafe_b64encode(msg.as_bytes())
+    raw = raw.decode()
+    body = {'raw': raw}
 
-service = googleapiclient.discovery.build('gmail', 'v1', credentials=creds)
+    message1 = body
+    message = (
+        service.users().messages().send(
+            userId="me", body=message1).execute())
+    print('Message Id: %s' % message['id'])
 
-my_email = 'menofmadisonffble@gmail.com'
-msg = MIMEMultipart('alternative')
-msg['Subject'] = 'MoM FFBL Weekly Rankings'
-msg['From'] = f'{my_email}'
-msg['To'] = f'{my_email}'
-msgPlain = 'This is my first email!'
-msgHtml = '<b>This is my first email!</b>'
-msg.attach(MIMEText(msgPlain, 'plain'))
-msg.attach(MIMEText(msgHtml, 'html'))
-raw = base64.urlsafe_b64encode(msg.as_bytes())
-raw = raw.decode()
-body = {'raw': raw}
+except HttpError as error:
+    print(f'An error occurred: {error}')
 
-message1 = body
-message = (
-    service.users().messages().send(
-        userId="me", body=message1).execute())
-print('Message Id: %s' % message['id'])
